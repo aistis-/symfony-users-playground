d774234cdd6c5c878c8b6c6ff0bb14c2fae0f063
Aistis S <aistis8624@gmail.com>
2014-11-24 11:51:06 +0100

    Add an empty AppBundle

---
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ */
+class AppExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
+    }
+}
